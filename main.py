c338304d2801ebf796eb878192ec05a8b15f672b
AliksBond <sashabond@protonmail.com>
2025-11-13 19:43:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+"""
+Точка входу для запуску персонального асистента.
+"""
+
 from assistant import PersonalAssistant
+
 
 def main():
     assistant = PersonalAssistant()
     assistant.run()
 
+
 if __name__ == "__main__":
     main()
+
 
